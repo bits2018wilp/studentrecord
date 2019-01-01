15d19974654d02f72fb977166e5aac81fa8fb9a8
bits2018wilp <46282728+bits2018wilp@users.noreply.github.com>
2019-01-01 10:45:02 +0530

    base commit

---
+++ src/main/java/org/bits/wilp/dsad/StudentHash.java
@@ -0,0 +1,182 @@
+package org.bits.wilp.dsad;
+
+import java.util.*;
+
+public class StudentHash {
+
+    private List<StudentRecord> studentRecordTable[];
+    private float totalRecords;
+    private int tableSize;
+    private int rehashCount;
+    private float bucketUsed;
+
+    public StudentHash(int tableSize) {
+        this.tableSize = tableSize;
+        studentRecordTable = new LinkedList[tableSize];
+    }
+
+    public StudentRecord get(String studentId) {
+        int tableIndex = getTableIndex(HashId(studentId));
+        List<StudentRecord> studentRecordList = studentRecordTable[tableIndex];
+
+        StudentRecord studentRecord = null;
+
+        if(studentRecordList != null && !studentRecordList.isEmpty()) {
+            for(StudentRecord str : studentRecordList) {
+                if(str.getStudentId().equals(studentId)) {
+                    studentRecord = str;
+                }
+            }
+        }
+        if(studentRecord == null) {
+            throw  new NoSuchElementException("record not found for "+ studentId);
+        }
+        return studentRecord;
+    }
+
+    public void put(String key, float value) {
+        put(new StudentRecord(key, value));
+    }
+
+    public void put(StudentRecord studentRecord) {
+        put(studentRecord, studentRecordTable);
+      //  System.out.println("TotalRecords: "+ totalRecords + " LoadFactor: "+ (totalRecords/tableSize) +" wasteFactor: "+ ( (tableSize-bucketUsed)/tableSize ));
+        if(totalRecords/tableSize > 0.75) {
+            reHash();
+        }
+    }
+
+    public void put(StudentRecord studentRecord, List<StudentRecord>[] studentRecordTable) {
+
+       // System.out.println("inserting: "+ studentRecord);
+        int index = getTableIndex(studentRecord.hashCode());
+        List<StudentRecord> studentRecordList = null;
+
+        studentRecordList = studentRecordTable[index];
+        if(studentRecordList == null) {
+            studentRecordList = new LinkedList<>();
+            studentRecordTable[index] = studentRecordList;
+           // System.out.println("bucket "+ index +" initialized..");
+            bucketUsed++;
+        }
+        studentRecordList.add(studentRecord);
+        totalRecords++;
+    }
+
+    public List<String> getAllKeys() {
+        List<String> keys = new LinkedList<>();
+
+        for(List<StudentRecord> list : studentRecordTable) {
+            if(list != null) {
+                for(StudentRecord str : list) {
+                    keys.add(str.getStudentId());
+                }
+            }
+        }
+        return keys;
+    }
+
+    private int getTableIndex(int hashCode) {
+        return (Math.abs(hashCode)%tableSize);
+    }
+
+    private void reHash() {
+
+        tableSize = tableSize * 2;
+        totalRecords =0;
+        bucketUsed = 0.0f;
+        List<StudentRecord> tmp[] = new LinkedList[tableSize];
+
+       // System.out.println("rehashing " + (rehashCount++) + " with Size=  " + studentRecordTable.length);
+
+        for(List<StudentRecord> list : studentRecordTable) {
+            if(list != null) {
+                for(StudentRecord studentRecord : list) {
+                    put(studentRecord, tmp);
+                }
+            }
+        }
+        studentRecordTable = tmp;
+    }
+
+    public void deleteAll() {
+        studentRecordTable = null;
+    }
+
+
+    public int HashId(String studentId) {
+        return Objects.hash(studentId); // hashing only on year & dept code
+    }
+
+    public Iterator<StudentRecord> getElementsIterator() {
+            return new RecordIterator(studentRecordTable);
+    }
+
+    private class RecordIterator implements Iterator<StudentRecord> {
+
+        int tableIndex = 0;
+        int currentListIndex=0;
+
+        List<StudentRecord> currentList = null;
+        List<StudentRecord> studentRecordTable[];
+
+        public RecordIterator( List<StudentRecord> studentRecordTable[] ) {
+            this.studentRecordTable = studentRecordTable;
+        }
+
+        @Override
+        public boolean hasNext() {
+
+            if(tableIndex < studentRecordTable.length) {
+
+                if( currentList !=  null && currentListIndex < currentList.size()) {
+                    return true;
+                }
+
+                currentListIndex = 0;
+                while( (currentList = studentRecordTable[tableIndex++]) == null && tableIndex < tableSize ) {
+                    //keep iterating untill it find non null list or exhaust the table
+                }
+                return currentList == null? false : true;
+            }
+            else
+                return false;
+        }
+
+        @Override
+        public StudentRecord next() {
+             return currentList.get(currentListIndex++);
+        }
+    }
+
+    public class StudentRecord {
+        private String studentId;
+        private float cgpa;
+
+        public StudentRecord(String studentId, float cgpa) {
+            this.studentId = studentId;
+            this.cgpa = cgpa;
+        }
+
+        @Override
+        public int hashCode() {
+            return HashId(studentId);
+        }
+
+        public String getStudentId() {
+            return studentId;
+        }
+
+        public float getCgpa() {
+            return cgpa;
+        }
+
+        @Override
+        public String toString() {
+            return "StudentRecord{" +
+                    "studentId='" + studentId + '\'' +
+                    ", cgpa=" + cgpa +
+                    '}';
+        }
+    }
+}
---
+++ src/main/java/org/bits/wilp/dsad/StudentRecords.java
@@ -0,0 +1,227 @@
+package org.bits.wilp.dsad;
+
+import java.io.*;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class StudentRecords {
+
+    private StudentHash studentHashTable;
+    public static final int curentYear = 2018;
+
+    public static void main(String[] args) throws IOException {
+
+        //generateInput(); // utiltity to generate random input file
+
+        StudentRecords studentRecords = new StudentRecords();
+        studentRecords.initHashTable(); // initialize StudentHash with size 1000 buckets
+
+        BufferedReader bfr = getInputFileReader(); // get input file reader. it has about 36k lines. 9000 records per dept.
+
+        String line = null;
+        while ((line = bfr.readLine()) != null) { // read the file until EOF
+            String[] split = line.split(","); // line contains studentId,cgpa. split it into 2 parts.
+            studentRecords.insertStudentRec(studentRecords.studentHashTable, split[0], Float.valueOf(split[1]));
+        }
+        bfr.close();
+
+        studentRecords.hallOfFame(studentRecords.studentHashTable, 6.0f);
+        studentRecords.newCourseList(studentRecords.studentHashTable, 6.0f, 9.9f);
+
+        studentRecords.getDeptWiseMaxAvg(studentRecords.studentHashTable);
+
+        //studentRecords.getGreatAlumnai(studentRecords.studentHashTable, 7.0f);
+
+    }
+
+
+    private void initHashTable() {
+        studentHashTable = new StudentHash(1000);
+    }
+
+
+    public void insertStudentRec(StudentHash studentHashTable, String studentId, float value) {
+        studentHashTable.put(studentId, value);
+    }
+
+    public void hallOfFame(StudentHash studentHashTable, float cgpa) throws FileNotFoundException {
+
+        String halloffameFile = System.getProperty("java.io.tmpdir")+ "halloffame.txt";
+
+        PrintWriter pw = new PrintWriter(new File(halloffameFile));
+
+        List<StudentHash.StudentRecord> hallOfFame = new LinkedList<>();
+        Iterator<StudentHash.StudentRecord> elementsIterator = studentHashTable.getElementsIterator();
+        while (elementsIterator.hasNext()) {
+            StudentHash.StudentRecord studentRecord = elementsIterator.next();
+            if (studentRecord != null && studentRecord.getCgpa() > cgpa) {
+                pw.println(studentRecord.getStudentId()+","+studentRecord.getCgpa());
+                hallOfFame.add(studentRecord);
+            }
+        }
+        pw.close();
+        System.out.println("hall of Fame has " + hallOfFame.size() + " records. Output is available at: "+ halloffameFile);
+    }
+
+    public void newCourseList(StudentHash studentHashTable, float mincgpa, float maxcgpa) throws FileNotFoundException {
+
+        List<StudentHash.StudentRecord> newCourse = new LinkedList<>();
+        Iterator<StudentHash.StudentRecord> elementsIterator = studentHashTable.getElementsIterator();
+
+        String courseOfferFile = System.getProperty("java.io.tmpdir")+ "courseOffer.txt";
+        PrintWriter pw = new PrintWriter(new File(courseOfferFile));
+
+        while (elementsIterator.hasNext()) {
+            StudentHash.StudentRecord studentRecord = elementsIterator.next();
+            if (studentRecord != null &&
+                    curentYear - getAdmissionYear(studentRecord.getStudentId()) >= 5 &&
+                    studentRecord.getCgpa() > mincgpa && studentRecord.getCgpa() < maxcgpa) {
+                pw.println(studentRecord.getStudentId()+","+studentRecord.getCgpa());
+                newCourse.add(studentRecord);
+            }
+        }
+        pw.close();
+        System.out.println("new course offering has " + newCourse.size() + " records. Output is available at: "+ courseOfferFile);
+
+    }
+
+    public void getDeptWiseMaxAvg(StudentHash studentHashTable) throws FileNotFoundException {
+
+        String departmentAverageFile = System.getProperty("java.io.tmpdir")+ "departmentAverage.txt";
+        PrintWriter pw = new PrintWriter(new File(departmentAverageFile));
+
+        float csMax = 0.0f;
+        float csTotalcgpa = 0.0f;
+        int csTotalStudents = 0;
+
+        float eceMax = 0.0f;
+        float eceTotalcgpa = 0.0f;
+        int eceTotalStudents = 0;
+
+        float mecMax = 0.0f;
+        float mecTotalcgpa = 0.0f;
+        int mecTotalStudents = 0;
+
+        float arcMax = 0.0f;
+        float arcTotalcgpa = 0.0f;
+        int arcTotalStudents = 0;
+
+        Iterator<StudentHash.StudentRecord> elementsIterator = studentHashTable.getElementsIterator();
+
+        while (elementsIterator.hasNext()) {
+            StudentHash.StudentRecord studentRecord = elementsIterator.next();
+            if (studentRecord != null) {
+                String dept = getDeptCode(studentRecord.getStudentId());
+                switch (dept) {
+                    case "CSE" :
+                        csMax = Math.max(csMax, studentRecord.getCgpa());
+                        csTotalcgpa += studentRecord.getCgpa();
+                        csTotalStudents++;
+                        break;
+                    case "ECE" :
+                        eceMax = Math.max(eceMax, studentRecord.getCgpa());
+                        eceTotalcgpa += studentRecord.getCgpa();
+                        eceTotalStudents++;
+                        break;
+                    case "MEC" :
+                        mecMax = Math.max(mecMax, studentRecord.getCgpa());
+                        mecTotalcgpa += studentRecord.getCgpa();
+                        mecTotalStudents++;
+                        break;
+                    case "ARC" :
+                        arcMax = Math.max(arcMax, studentRecord.getCgpa());
+                        arcTotalcgpa += studentRecord.getCgpa();
+                        arcTotalStudents++;
+                        break;
+                }
+            }
+        }
+        System.out.println("Dept: CSE  TotalStudents: " + csTotalStudents + " Max: " + csMax + " Avg: " + (csTotalcgpa / csTotalStudents));
+        System.out.println("Dept: ECE  TotalStudents: " + eceTotalStudents + " Max: " + eceMax + " Avg: " + (eceTotalcgpa / eceTotalStudents));
+        System.out.println("Dept: MEC  TotalStudents: " + mecTotalStudents + " Max: " + mecMax + " Avg: " + (mecTotalcgpa / mecTotalStudents));
+        System.out.println("Dept: ARC  TotalStudents: " + arcTotalStudents + " Max: " + arcMax + " Avg: " + (arcTotalcgpa / arcTotalStudents));
+
+        pw.println("CSE," + csMax + "," + (csTotalcgpa / csTotalStudents));
+        pw.println("ECE," + eceMax + "," + (eceTotalcgpa / eceTotalStudents));
+        pw.println("MEC," + mecMax + "," + (mecTotalcgpa / mecTotalStudents));
+        pw.println("ARC," + arcMax + "," + (arcTotalcgpa / arcTotalStudents));
+        pw.close();
+
+        System.out.println("Dept wise Max/Avg Output is available at: "+ departmentAverageFile);
+
+    }
+
+    public void getGreatAlumnai(StudentHash studentHashTable, float cgpa) {
+
+        List<StudentHash.StudentRecord> greatAlumnai = new LinkedList<>();
+
+        Iterator<StudentHash.StudentRecord> elementsIterator = studentHashTable.getElementsIterator();
+        while (elementsIterator.hasNext()) {
+            StudentHash.StudentRecord studentRecord = elementsIterator.next();
+            if (studentRecord.getCgpa() > cgpa) {
+                greatAlumnai.add(studentRecord);
+            }
+        }
+        System.out.println("\n greatAlumnai: " + greatAlumnai);
+    }
+
+    public String getDeptCode(String studentID) {
+        return studentID.substring(4, 7);
+    }
+
+    private int getAdmissionYear(String studentId) {
+        return Integer.valueOf(studentId.substring(0, 4));
+    }
+
+    public static String getRandomDept(int i) {
+        //   int i = 1 + (int)(Math.random() * (4-0) + 1);
+
+        switch (i) {
+            case 1:
+                return "CSE";
+            case 2:
+                return "ECE";
+            case 3:
+                return "MEC";
+            case 4:
+                return "ARC";
+        }
+        return "CSE";
+    }
+
+    public static float getRandomCGPA(Random random) {
+        float result = random.nextFloat() * (10 - 1);
+        return result;
+    }
+
+    private static int getRandomYear() {
+        int i = 2000 + (int) (Math.random() * (2019 - 2000) + 1);
+        return i;
+    }
+
+
+    private static BufferedReader getInputFileReader() throws FileNotFoundException {
+        URL resource = ClassLoader.getSystemClassLoader().getResource("input/Input.txt");
+        File file = new File(resource.getPath());
+        BufferedReader bfr = new BufferedReader(new FileReader(file));
+        return bfr;
+    }
+
+    private static void generateInput() throws FileNotFoundException {
+
+        Random random = new Random();
+        PrintWriter pw = new PrintWriter(new File("D:/Input.txt"));
+        for (int i = 1; i <= 4; i++) {
+            int r = 1000;
+            for (int j = 1000; j <= 9999; j++) {
+                String line = getRandomYear() + getRandomDept(i) + r++ + "," + getRandomCGPA(random);
+                pw.println(line);
+            }
+        }
+        pw.close();
+    }
+
+}
