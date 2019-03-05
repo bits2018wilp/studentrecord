d79a24003c796ce8482667540136665dabee6132
bits2018wilp <46282728+bits2018wilp@users.noreply.github.com>
2019-03-05 06:57:49 +0530

    DSAD2 bug fix

--- src/main/java/org/bits/wilp/dp/CourseDistribution.java
+++ src/main/java/org/bits/wilp/dp/CourseDistribution.java
@@ -15,20 +15,32 @@ public class CourseDistribution {
         students.add("kasif");
         students.add("pp");
         students.add("pk");
-
-        /*students.add("umang");
         students.add("mk");
+        students.add("umang");
+
+/*
         students.add("krishna");
         students.add("sachin");
         students.add("kannan");
-        students.add("rangith");
+        students.add("rengith");
         students.add("abhinav");
 */
+
         List<String> courses = new ArrayList<>();
         courses.add("nlp");
         courses.add("ai");
         courses.add("data mining");
         courses.add("big-data");
+        courses.add("spatial data analysis");
+        courses.add("image processing");
+
+/*
+        courses.add("machine learning");
+        courses.add("graph mining");
+        courses.add("e commerce");
+        courses.add("wireless mobile comm");
+        courses.add("cloud computing");
+*/
 
         Map<String, List<String>> choice = new HashMap<>();
 
@@ -47,6 +59,38 @@ public class CourseDistribution {
         List<String> choice4 = new ArrayList<>();
         choice4.add("nlp"); choice4.add("ai");
         choice.put("pk", choice4);
+
+        List<String> choice5 = new ArrayList<>();
+        choice5.add("spatial data analysis"); choice5.add("image processing");
+        choice.put("mk", choice5);
+
+        List<String> choice6 = new ArrayList<>();
+        choice6.add("data mining"); choice6.add("image processing");
+        choice.put("umang", choice6);
+
+/*
+
+
+        List<String> choice7 = new ArrayList<>();
+        choice7.add("wireless mobile comm"); choice7.add("e commerce");
+        choice.put("krishna", choice7);
+
+        List<String> choice8 = new ArrayList<>();
+        choice8.add("cloud computing"); choice8.add("nlp");
+        choice.put("rengith", choice8);
+
+        List<String> choice9 = new ArrayList<>();
+        choice9.add("machine learning"); choice9.add("image processing");
+        choice.put("abhinav", choice9);
+
+        List<String> choice10 = new ArrayList<>();
+        choice10.add("big-data"); choice10.add("cloud computing");
+        choice.put("sachin", choice10);
+
+        List<String> choice11 = new ArrayList<>();
+        choice11.add("spatial data analysis"); choice11.add("wireless mobile comm");
+        choice.put("kannan", choice11);
+*/
 
 /*
         List<String> choice1 = new ArrayList<>();
@@ -81,17 +125,18 @@ public class CourseDistribution {
 
     public void assign(List<String> students, List<String> courses, Map<String, List<String>> choice, List<String> assignment, AtomicInteger counter ) {
 
-        if(assignment != null && assignment.size() == 4)
+      //  if(assignment != null && assignment.size() == 11)
             System.out.println(assignment);
 
         if(students == null || students.isEmpty() || courses == null || courses.isEmpty() )
             return;
 
-        for(String st : students) {
+   //     for(String st : students) {
 
-           if (studentIsAssignedACourse(st, assignment)) {
-               continue;
-           }
+            String st = students.get(0);
+            if (studentIsAssignedACourse(st, assignment)) {
+                return;
+            }
 
             for (String c : choice.get(st)) {
 
@@ -101,7 +146,7 @@ public class CourseDistribution {
                 else
                     tmpAssignment = new ArrayList<>(assignment);
 
-                if (courses.contains(c) )  {
+                if (courses.contains(c) && courseIsNotAssigned(c, tmpAssignment))  {
 
                     tmpAssignment.add(st + "#" + c);
                     counter.incrementAndGet();
@@ -117,7 +162,20 @@ public class CourseDistribution {
                     assign(studentLeft, courseLeft, choice, tmpAssignment, counter);
                 }
             }
+       // }
+    }
+
+    private boolean courseIsNotAssigned(String c, List<String> tmpAssignment) {
+
+        if (tmpAssignment == null)
+            return false;
+
+        for(String ass : tmpAssignment) {
+            if(ass.contains(c)) {
+                return false;
+            }
         }
+        return true;
     }
 
     private boolean studentIsAssignedACourse(String st, List<String> localAssignment) {
